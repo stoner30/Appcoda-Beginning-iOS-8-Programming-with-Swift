847741d2022b82230f240010a1d5fa307f9e82dd
Stoner Wang <stoner.wong@gmail.com>
2015-08-26 09:57:36 +0800

    Chapter 06: Customize Table View Using Prototype Cell

---
+++ FoodPin/FoodPin/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  FoodPin
+//
+//  Created by Stoner Wang on 15/8/26.
+//  Copyright (c) 2015年 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
