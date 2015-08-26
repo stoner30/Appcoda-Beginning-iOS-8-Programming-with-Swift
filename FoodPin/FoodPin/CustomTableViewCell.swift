a29fc8cdd78598a33a47b7d1fcb0b47c703ff7c5
Stoner Wang <stoner.wong@gmail.com>
2015-08-26 13:45:31 +0800

    Chapter 07: Table Cell Selection and UIAlertController

--- FoodPin/FoodPin/CustomTableViewCell.swift
+++ FoodPin/FoodPin/CustomTableViewCell.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class CustomTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- FoodPin/FoodPin/RestaurantTableTableViewController.swift
+++ FoodPin/FoodPin/RestaurantTableTableViewController.swift
@@ -10,6 +10,48 @@ import UIKit
 
 class RestaurantTableTableViewController: UITableViewController {
 
+    var restaurantNames = [
+        "Cafe Deadend", "Homei", "Teakha",
+        "Cafe Loisl", "Petite Oyster", "For Kee Restaurant",
+        "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate",
+        "Palomino Espresso", "Upstate", "Traif",
+        "Graham Avenue Meats", "Waffle & Wolf", "Five Leaves",
+        "Cafe Lore", "Confessional", "Barrafina",
+        "Donostia", "Royal Oak", "Thai Cafe"
+    ]
+    
+    var restaurantImages = [
+        "cafedeadend.jpg", "homei.jpg", "teakha.jpg",
+        "cafeloisl.jpg", "petiteoyster.jpg", "forkeerestaurant.jpg",
+        "posatelier.jpg", "bourkestreetbakery.jpg", "haighschocolate.jpg",
+        "palominoespresso.jpg", "upstate.jpg", "traif.jpg",
+        "grahamavenuemeats.jpg", "wafflewolf.jpg", "fiveleaves.jpg",
+        "cafelore.jpg", "confessional.jpg", "barrafina.jpg",
+        "donostia.jpg", "royaloak.jpg", "thaicafe.jpg"
+    ]
+    
+    var restaurantLocations = [
+        "Hong Kong", "Hong Kong", "Hong Kong",
+        "Hong Kong", "Hong Kong", "Hong Kong",
+        "Hong Kong", "Sydney", "Sydney",
+        "Sydney", "New York", "New York",
+        "New York", "New York", "New York",
+        "New York", "New York", "London",
+        "London", "London", "London"
+    ]
+    
+    var restaurantTypes = [
+        "Coffee & Tea Shop", "Cafe", "Tea House",
+        "Austrian / Causual Drink", "French", "Bakery",
+        "Bakery", "Chocolate", "Cafe",
+        "American / Seafood", "American", "American",
+        "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea",
+        "Latin American", "Spanish", "Spanish",
+        "Spanish", "British", "Thai"
+    ]
+    
+    var restaurantIsVisited = [Bool](count: 21, repeatedValue: false)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,72 +68,59 @@ class RestaurantTableTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
-        return 0
-    }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
-        return 0
+        return self.restaurantNames.count
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
 
-    /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! CustomTableViewCell
 
         // Configure the cell...
+        cell.nameLabel.text = self.restaurantNames[indexPath.row]
+        cell.thumbnailImageView.image = UIImage(named: self.restaurantImages[indexPath.row])
+        cell.thumbnailImageView.layer.cornerRadius = cell.thumbnailImageView.frame.width / 2
+        cell.thumbnailImageView.clipsToBounds = true
+        cell.locationLabel.text = self.restaurantLocations[indexPath.row]
+        cell.typeLabel.text = self.restaurantTypes[indexPath.row]
+        cell.accessoryType = restaurantIsVisited[indexPath.row] ? .Checkmark : .None
 
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
+    // MASK: - Table view delegate
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let optionMenu = UIAlertController(title: nil, message: "What do you want to do?", preferredStyle: UIAlertControllerStyle.ActionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+        optionMenu.addAction(cancelAction)
+        
+        let callActionHandler = { (action: UIAlertAction!) -> Void in
+            let alertMessage = UIAlertController(title: "Service unavailable", message: "Sorry, the call feature is not available yet. Please retry later.", preferredStyle: .Alert)
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            self.presentViewController(alertMessage, animated: true, completion: nil)
+        }
+        let callAction = UIAlertAction(title: "Call 123-000-\(indexPath.row)", style: .Default, handler: callActionHandler)
+        optionMenu.addAction(callAction)
+        
+        let isVisitedTitle = "I've" + (self.restaurantIsVisited[indexPath.row] ? " not" : "") + " been here"
+        let isVisitedAccessoryType = self.restaurantIsVisited[indexPath.row]
+            ? UITableViewCellAccessoryType.None
+            : UITableViewCellAccessoryType.Checkmark
+        let isVisitedAction = UIAlertAction(title: isVisitedTitle, style: .Default, handler: { (action: UIAlertAction!) -> Void in
+            let cell = tableView.cellForRowAtIndexPath(indexPath)
+            cell?.accessoryType = isVisitedAccessoryType
+            self.restaurantIsVisited[indexPath.row] = !self.restaurantIsVisited[indexPath.row]
+        })
+        optionMenu.addAction(isVisitedAction)
+        
+        self.presentViewController(optionMenu, animated: true, completion: nil)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
