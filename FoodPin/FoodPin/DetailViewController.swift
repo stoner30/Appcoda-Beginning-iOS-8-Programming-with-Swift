9496936c51f4a4db65c5a8fcc014f0de105a5c28
Stoner Wang <stoner.wong@gmail.com>
2015-08-27 11:30:05 +0800

    Chapter 12: Self Sizing Cells

--- FoodPin/FoodPin/DetailViewController.swift
+++ FoodPin/FoodPin/DetailViewController.swift
@@ -25,6 +25,8 @@ class DetailViewController: UIViewController, UITableViewDataSource, UITableView
         self.tableView.backgroundColor = UIColor(red: 240.0 / 255.0, green: 240.0 / 255.0, blue: 240.0 / 255.0, alpha: 0.2)
         self.tableView.tableFooterView = UIView(frame: CGRectZero)
         self.tableView.separatorColor = UIColor(red: 240.0 / 255.0, green: 240.0 / 255.0, blue: 240.0 / 255.0, alpha: 0.8)
+        self.tableView.estimatedRowHeight = 36.0
+        self.tableView.rowHeight = UITableViewAutomaticDimension
     }
 
     override func didReceiveMemoryWarning() {
